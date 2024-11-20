a9721113c4aab551b805e43ad9c8f9610ffc7302
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2024-11-20 12:45:57 +0700

    feat(Molecules-LoginHeader): [KLDG-92] Create header and branding section

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 
 const Images = {
-  Logo: require('./Logo.jpeg'),
   LogoTransparent: require('./LogoTransparent.png'),
   Background: require('./background.png'),
 };
