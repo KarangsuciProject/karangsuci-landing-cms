317e7db5db9cc44ec9c79224630bd0de13264365
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2024-11-29 00:35:34 +0700

    fix(Pages-Login): [KLDG-94] adjust xl breakpoint responsiveness

--- src/containers/Auth/LoginPage.container.tsx
+++ src/containers/Auth/LoginPage.container.tsx
@@ -2,7 +2,7 @@ import { Organism } from '../../components';
 
 const LoginPage = () => {
   return (
-    <div className="h-[100vh] flex px-8 lg:px-64 py-12 md:py-8 bg-semiWhiteBg">
+    <div className="h-[100vh] flex px-8 xl:px-72 py-12 md:py-8 bg-semiWhiteBg">
       <Organism.LeftSide />
       <Organism.RightSide />
     </div>
