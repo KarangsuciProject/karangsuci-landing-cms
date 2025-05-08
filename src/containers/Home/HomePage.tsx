fb2ba1e0f9e8324a7f35be794e87a624e3262e84
Febryan Hernanda <febryanhernanda@Febryans-MacBook-Pro.local>
2025-05-08 11:51:29 +0700

    feat(Container):[KLCMS-9] added a HomePage Container

---
+++ src/containers/Home/HomePage.tsx
@@ -0,0 +1,15 @@
+function HomePage() {
+  return (
+    <div className="flex flex-col gap-5">
+      <h1 className="text-3xl">Dasboard</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quia
+        consectetur dolores! Inventore, obcaecati. Facilis amet voluptatibus
+        fugit impedit commodi sequi consequatur accusamus dolores, velit quis
+        eaque. Autem, illo ea?
+      </p>
+    </div>
+  );
+}
+export default HomePage;
