ce59cdc77984f647a10a765ca32ef6c99564c208
Febryan Hernanda <febryanhernanda@Mac.lan>
2025-06-12 15:52:59 +0700

    fix(HeroForm): [KLCMS-13] fix import dispatch path

--- src/components/molecules/HeroForm/HeroForm.component.tsx
+++ src/components/molecules/HeroForm/HeroForm.component.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Alert, Typography } from '@mui/material';
 
-import { useAppDispatch } from '../../../redux/hooks';
+import { useAppDispatch } from '../../../redux';
 import { addHeroData } from '../../../redux/LandingSlice';
 import {
   ButtonComponent,
