e7b91132ef2ec22a2d23deeff28f2e4d7a986d49
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2025-06-19 22:51:45 +0700

    feat(auth): refactor logout functionality and update action names

--- src/redux/auth/auth.slice.ts
+++ src/redux/auth/auth.slice.ts
@@ -1,7 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-import reducers from './auth.reducers';
-import { authenticateUser } from './auth.action';
+import { authReducers, logoutReducers } from './auth.reducers';
 import type { ILoginState } from './auth.types';
 
 const initialState: ILoginState = {
@@ -14,40 +13,12 @@ const initialState: ILoginState = {
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: reducers,
+  reducers: {},
   extraReducers: builder => {
-    builder.addCase(authenticateUser.pending, state => {
-      state.loading = true;
-    });
-    builder.addCase(authenticateUser.fulfilled, (state, action) => {
-      state.loading = false;
-      state.user = action.payload.user._doc;
-      state.isLoggedIn = true;
-      state.error = '';
-
-      localStorage.setItem(
-        'authToken',
-        action.payload?.tokens?.access?.token || ''
-      );
-      localStorage.setItem(
-        'refreshToken',
-        action.payload?.tokens?.refresh?.token || ''
-      );
-      localStorage.setItem(
-        'isStoreCredentials',
-        action.payload?.isStoreCredentials.toString() || 'false'
-      );
-    });
-    builder.addCase(authenticateUser.rejected, (state, action) => {
-      state.loading = false;
-      state.error =
-        action.payload && typeof action.payload === 'string'
-          ? action.payload
-          : 'An error occurred';
-    });
+    authReducers(builder);
+    logoutReducers(builder);
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { logout } = authSlice.actions;
 export const { reducer } = authSlice;
--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,11 +1,11 @@
-import { reducer as loginReducer } from './auth';
+import { reducer as authReducer } from './auth';
 import heroDataSlice from './LandingSlice/heroData/heroData.slice';
 import profileDataSlice from './LandingSlice/profileData/profileData.slice';
 import quoteDataSlice from './LandingSlice/quoteData/quoteData.slice';
 import footerDataSlice from './LandingSlice/footerData/footerData.slice';
 
 export default {
-  auth: loginReducer,
+  auth: authReducer,
   heroData: heroDataSlice,
   profileData: profileDataSlice,
   quoteData: quoteDataSlice,
